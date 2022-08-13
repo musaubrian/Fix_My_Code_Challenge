eacfa5ace4636297bd463b9cb04136208f78b201
musaubrian <musaubrian15@gmail.com>
2022-08-13 23:24:52 +0300

    update fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -16,10 +16,8 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-            if (i % 5) == 0:
-                tmp_result.append("FizzBuzz")
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
